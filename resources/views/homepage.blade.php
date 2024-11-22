f11c7af6900b7320debfc87e6c4ae43f719f3f5c
KenjiTonnaer <Kenjitonnaer11@gmail.com>
2024-11-22 09:50:46 +0100

    homepage kleienr aanpassingen

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,3 +1,4 @@
 <x-base-layout>
-    <h1>Home</h1>
+    <h1 class="flex justify-center text-3xl pt-10">Welkom bij het schoolvoetbal</h1>
+    <div class="hometext flex justify-center pt-16 ">test informatie niet op letten</div>
 </x-base-layout>
