a42abcb6669301ae82af74ebeefd11d291f95f86
DiegoVanV <159238577+DiegoVanV@users.noreply.github.com>
2024-11-25 09:25:15 +0100

    Update homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,11 +1,8 @@
 <x-base-layout>
     <h1 class="flex justify-center text-3xl pt-10">Welkom bij het schoolvoetbal</h1>
-    <div class="hometext flex justify-center pt-16 ">test informatie niet op letten</div>
-    <div class="bg-yellow-100 rounded-lg shadow-lg p-8 text-center mb-12">
-        <h1 class="text-4xl font-bold text-blue-600 mb-4">Welkom bij Bricky!</h1>
-        <p class="text-xl text-gray-700 mb-6">De beste plek om al je LEGO® sets te vinden.</p>
-        <a href="#" class="bg-red-600 text-white font-bold py-2 px-6 rounded hover:bg-red-700">
-            Shop nu
-        </a>
+
+    <div class="bg-gray-200 rounded-lg shadow-lg p-8 text-center mb-12">
+        <h1 class="text-4xl font-bold text-blue-600 mb-4">informatie:</h1>
+        <p class="text-xl text-gray-700 mb-6">Perferendis aspernatur temporibus sed tempora officia! Eum quisquam atque beatae aspernatur officiis, modi nesciunt vero id similique minus doloremque, laborum error qui?</p>
     </div>
 </x-base-layout>
