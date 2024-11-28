a743554a449f3d71d4a45afe29f229c21b30644d
KenjiTonnaer <Kenjitonnaer11@gmail.com>
2024-11-28 13:31:02 +0100

    contact pagina

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,12 @@
 <x-base-layout>
-    <h1>contact</h1>
+    <h1 class="flex justify-center text-3xl pt-10 pb-10">contact</h1>
+
+    <div class="bg-gray-200 rounded-lg shadow-lg p-8 flex flex-col justify-center items-center max-w-md mx-auto">
+        <div class="text-center">
+            <h2 class="text-2xl font-bold">Contact informatie</h2>
+            <p class="mt-4">E-mail: test@example.com</p>
+            <p class="mt-4">Telefoonnummer: +31 123 456 7890</p>
+            <p class="mt-4">Adres: Teststraat 123, 1234 AB, Testland</p>
+        </div>
+    </div>
 </x-base-layout>
