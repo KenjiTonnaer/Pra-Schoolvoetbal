0eb2b8b3fdd3f47a47ac3a3fa73e03f4ec119c22
DiegoVanV <159238577+DiegoVanV@users.noreply.github.com>
2024-11-29 11:13:18 +0100

    pagina beveiliging
    laat de gebruiker eerst moeten inloggen of registreren voordat ze een pagina kunnen gebruiken.
    (buiten homepage)

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,6 +1,17 @@
 <x-base-layout>
-    <h1 class="flex justify-center text-3xl pt-10 pb-10">contact</h1>
+    <h1 class="flex justify-center text-3xl pt-10 pb-10">contact pagina</h1>
+@guest
+    <div class="bg-gray-200 rounded-lg shadow-lg my-8 p-8 flex flex-col justify-center items-center max-w-md mx-auto">
+        <div class="text-center">
+            <a class="bg-blue-500  text-white text-lg font-semibold py-2 px-6 rounded hover:bg-blue-600" href="{{ route('login') }}" class="hover:underline">inloggen</a>
+            <a class="bg-blue-500  text-white text-lg font-semibold py-2 px-6 rounded hover:bg-blue-600" href="{{ route('register') }}" class="hover:underline">Registreren</a>
 
+
+            <p class="mt-4">Je moet eerst inloggen of een nieuw acount aanmaken voordat je deze pagina kunt gebruiken</p>
+        </div>
+    </div>
+@endguest
+@auth
     <div class="bg-gray-200 rounded-lg shadow-lg p-8 flex flex-col justify-center items-center max-w-md mx-auto">
         <div class="text-center">
             <h2 class="text-2xl font-bold">Contact informatie</h2>
@@ -9,4 +20,6 @@
             <p class="mt-4">Adres: Teststraat 123, 1234 AB, Testland</p>
         </div>
     </div>
+
+@endauth
 </x-base-layout>
--- resources/views/gokken.blade.php
+++ resources/views/gokken.blade.php
@@ -1,3 +1,17 @@
 <x-base-layout>
-    <h2>gokpagina</h2>
+    <h1 class="flex justify-center text-3xl pt-10 pb-10">gok pagina</h1>
+@guest
+    <div class="bg-gray-200 rounded-lg shadow-lg my-8 p-8 flex flex-col justify-center items-center max-w-md mx-auto">
+        <div class="text-center">
+            <a class="bg-blue-500  text-white text-lg font-semibold py-2 px-6 rounded hover:bg-blue-600" href="{{ route('login') }}" class="hover:underline">inloggen</a>
+            <a class="bg-blue-500  text-white text-lg font-semibold py-2 px-6 rounded hover:bg-blue-600" href="{{ route('register') }}" class="hover:underline">Registreren</a>
+
+
+            <p class="mt-4">Je moet eerst inloggen of een nieuw acount aanmaken voordat je deze pagina kunt gebruiken</p>
+        </div>
+    </div>
+@endguest
+@auth
+<h2>gokken maar!</h2>
+@endauth
 </x-base-layout>
