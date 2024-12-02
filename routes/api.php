42c6bcdbf498f32d88c93fbfd1c9b4090fa78140
iTsDante07 <84541745+iTsDante07@users.noreply.github.com>
2024-12-02 10:19:47 +0100

    api

--- routes/api.php
+++ routes/api.php
@@ -21,5 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::get('/api', [ApiController::class, 'index']);
+Route::get('/data', [ApiController::class, 'index']);
 
