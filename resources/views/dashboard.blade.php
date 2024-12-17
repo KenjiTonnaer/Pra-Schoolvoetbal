67ba1eb7bdada84ff10ae4e7ac0c2abce73e530a
DiegoVanV <159238577+DiegoVanV@users.noreply.github.com>
2024-12-17 12:35:16 +0100

    link dashboard - homepage nu zichtbaar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
-<a class=" text-white underline" href="/">Homepage</a>
+<a class=" text-black underline" href="/">Homepage</a>
     </x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
